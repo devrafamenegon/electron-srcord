cfd385f2984a5367229e54815664d174158549de
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-05-19 17:49:59 -0300

    :construction: Render button creation

---
+++ src/render.js
@@ -0,0 +1,30 @@
+const { desktopCapturer, remote } = require('electron');
+
+const { writeFile } = require('fs');
+
+const { dialog, Menu } = remote;
+
+// Global state
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+// Buttons
+const videoElement = document.querySelector('video');
+
+const startBtn = document.getElementById('startBtn');
+startBtn.onclick = e => {
+  mediaRecorder.start();
+  startBtn.classList.add('is-danger');
+  startBtn.innerText = 'Recording';
+};
+
+const stopBtn = document.getElementById('stopBtn');
+
+stopBtn.onclick = e => {
+  mediaRecorder.stop();
+  startBtn.classList.remove('is-danger');
+  startBtn.innerText = 'Start';
+};
+
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
